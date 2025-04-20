3e88192994328eb5cc2f5af0d1be842746d061bc
Iago <prog.iago.oliveira@gmail.com>
2025-04-20 00:02:27 -0300

    Criado pacote composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'oasys/web-components',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ff8436e57044d1db82b3d4135f78e8eaf7462d22',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'oasys/web-components' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ff8436e57044d1db82b3d4135f78e8eaf7462d22',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
